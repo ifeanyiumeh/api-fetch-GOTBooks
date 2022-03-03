8aaaada217af9d96f576d8fa955d608f4ff9ac50
Ifeanyi Umeh <somto_umeh@yahoo.com>
2022-03-03 15:22:28 -0500

    Finalized the challenge with return statemnts for the map and used useState to set the data from the fetch and pass to the book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [books, setbooks] = useState(null);
+  const apiURL = "https://www.anapioficeandfire.com/api/books?pageSize=30";
+
+  function fetchBooks() {
+    fetch(apiURL)
+      .then((resp) => resp.json())
+      .then((data) => setbooks(data));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Game of Thrones Books</h1>
+      <h2>Fetch a list from an API and display it</h2>
+
+      <div>
+        <button className="fetch-button" onClick={fetchBooks}>
+          Fetch Data
+        </button>
+        <br />
+      </div>
+
+      <div className="books">
+        {books &&
+          books.map((book, index) => {
+            const authors = book.authors.join(", ");
+            const cleanedDate = new Date(book.released).toDateString();
+
+            return (
+              <div className="book" key={index}>
+                <h3>Book {index + 1}</h3>
+                <h2>{book.name}</h2>
+
+                <div className="details">
+                  <p>👨: {authors}</p>
+                  <p>📖: {book.numberOfPages}</p>
+                  <p>🏘️: {book.country}</p>
+                  <p>⏰: {cleanedDate}</p>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
